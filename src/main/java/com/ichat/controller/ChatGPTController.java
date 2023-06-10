a1f693a7e8e88d8b83ed2d52f8f905801b500956
rviannati <rvianna.ti@gmail.com>
2023-06-10 10:20:27 -0300

    add voice

--- src/main/java/com/ichat/controller/ChatGPTController.java
+++ src/main/java/com/ichat/controller/ChatGPTController.java
@@ -3,6 +3,7 @@ package com.ichat.controller;
 import com.ichat.model.request.ChatBotInputRequest;
 import com.ichat.model.response.ChatGPTResponse;
 import com.ichat.service.ChatGPTService;
+import com.ichat.service.Espeak;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,8 @@ public class ChatGPTController {
 
     private ChatGPTService chatGPTService;
 
+    private Espeak espeak = new Espeak();
+
     public ChatGPTController(ChatGPTService chatGPTService) {
         this.chatGPTService = chatGPTService;
     }
@@ -21,6 +24,9 @@ public class ChatGPTController {
     @PostMapping("/chat")
     public ResponseEntity<ChatGPTResponse> processInputRequest(@RequestBody ChatBotInputRequest chatbotInputRequest) {
         ChatGPTResponse chatCPTResponse = chatGPTService.getChatGPTResponse(chatbotInputRequest.getMessage());
+
+        chatCPTResponse.getChoices().forEach(voice -> espeak.speak(voice.getMessage().content));
+//        espeak.speak(chatCPTResponse.getUsage().);
         return new ResponseEntity<>(chatCPTResponse, HttpStatus.OK);
     }
 }
